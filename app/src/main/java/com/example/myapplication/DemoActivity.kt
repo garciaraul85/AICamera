8573cea1816d48da44e41a666dc69b21fc6a5841
raulgarcia <garcia.raul@gmail.com>
2024-05-18 22:26:13 -0400

    Added support to voice using eleven labs api

--- app/src/main/java/com/example/myapplication/DemoActivity.kt
+++ app/src/main/java/com/example/myapplication/DemoActivity.kt
@@ -8,6 +8,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.Manifest
 import android.content.pm.PackageManager
+import android.media.MediaPlayer
 import android.util.Log
 import android.widget.ImageView
 import androidx.camera.core.CameraSelector
@@ -17,6 +18,23 @@ import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.pixelcarrot.base64image.Base64Image
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.Headers
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import kotlin.coroutines.resumeWithException
 
 class DemoActivity : AppCompatActivity() {
     private lateinit var previewView: PreviewView
@@ -29,7 +47,10 @@ class DemoActivity : AppCompatActivity() {
         previewView = findViewById(R.id.previewView)
         imageView = findViewById(R.id.imageView)
 
-        initCameraOrPermissions()
+//        initCameraOrPermissions()
+        CoroutineScope(Dispatchers.IO).launch {
+            textToSpeech()
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -92,6 +113,90 @@ class DemoActivity : AppCompatActivity() {
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    val client = OkHttpClient()
+
+    val CHUNK_SIZE = 1024
+    val XI_API_KEY = ""
+    val VOICE_ID = ""
+    val TEXT_TO_SPEAK = "Hello world!"
+    val OUTPUT_FILENAME = "output.mp3"
+
+    private suspend fun textToSpeech() {
+        val ttsUrl = "https://api.elevenlabs.io/v1/text-to-speech/$VOICE_ID/stream"
+
+        val headers = Headers.Builder()
+            .add("Accept", "application/json")
+            .add("xi-api-key", XI_API_KEY)
+            .build()
+
+        val json = """
+            {
+                "text": "$TEXT_TO_SPEAK",
+                "model_id": "eleven_multilingual_v2",
+                "voice_settings": {
+                    "stability": 0.5,
+                    "similarity_boost": 0.8,
+                    "style": 0.0,
+                    "use_speaker_boost": true
+                }
+            }
+        """.trimIndent()
+
+        val requestBody = json.toRequestBody("application/json".toMediaType())
+        val request = Request.Builder()
+            .url(ttsUrl)
+            .post(requestBody)
+            .headers(headers)
+            .build()
+
+        try {
+            val response = client.newCall(request).await()
+
+            if (response.isSuccessful) {
+                response.body?.let { responseBody ->
+                    val file = File(filesDir, OUTPUT_FILENAME)
+                    FileOutputStream(file).use { outputStream ->
+                        responseBody.byteStream().use { inputStream ->
+                            val buffer = ByteArray(CHUNK_SIZE)
+                            var bytesRead: Int
+                            while (inputStream.read(buffer).also { bytesRead = it } != -1) {
+                                outputStream.write(buffer, 0, bytesRead)
+                            }
+                        }
+                    }
+                    playAudio(file)
+                } ?: println("Failed to get response body.")
+            } else {
+                println("Request failed: ${response.message}")
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    suspend fun Call.await(): Response = suspendCancellableCoroutine { continuation ->
+        enqueue(object : Callback {
+            override fun onResponse(call: Call, response: Response) {
+                continuation.resume(response) {}
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                continuation.resumeWithException(e)
+            }
+        })
+    }
+
+    private fun playAudio(file: File) {
+        val mediaPlayer = MediaPlayer()
+        mediaPlayer.setDataSource(file.path)
+        mediaPlayer.prepare()
+        mediaPlayer.start()
+        mediaPlayer.setOnCompletionListener {
+            file.delete()
+        }
     }
 
     companion object {
