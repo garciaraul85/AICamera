96e1464fe0afc27531e18ff2330d010f50326bca
raulgarcia <garcia.raul@gmail.com>
2024-05-23 22:26:53 -0400

    using open ai for text2speech

--- app/src/main/java/com/example/myapplication/DemoActivity.kt
+++ app/src/main/java/com/example/myapplication/DemoActivity.kt
@@ -4,6 +4,8 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
@@ -29,6 +31,7 @@ import okhttp3.Headers
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.RequestBody
 import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
 import java.io.File
@@ -55,13 +58,13 @@ class DemoActivity : AppCompatActivity() {
 
         val buttonRecord: Button = findViewById(R.id.button_record)
 
-
         imageAnalyzer = MyImageAnalyzer(imageDescriptionDao, { base64Image ->
             handleImage(base64Image)
         }, { answer ->
             CoroutineScope(Dispatchers.IO).launch {
                 Log.d("SpeechRecognizerManager", "textToSpeech: $answer")
-                textToSpeech(answer)
+//                textToSpeech(answer)
+                textToSpeechGPT(answer)
             }
         })
 
@@ -180,45 +183,50 @@ class DemoActivity : AppCompatActivity() {
         ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
     }
 
-    val client = OkHttpClient()
-
     val CHUNK_SIZE = 1024
     val XI_API_KEY = ""
     val VOICE_ID = ""
     val OUTPUT_FILENAME = "output.mp3"
+    val bitrate = 128000
+    private val API = ""
 
-    private suspend fun textToSpeech(text: String) {
-        val ttsUrl = "https://api.elevenlabs.io/v1/text-to-speech/$VOICE_ID/stream"
+    private suspend fun textToSpeechGPT(text: String) {
+        val client = OkHttpClient()
+        val mediaType = "application/json".toMediaType()
 
-        val headers = Headers.Builder()
-            .add("Accept", "application/json")
-            .add("xi-api-key", XI_API_KEY)
-            .build()
+        val escapedText = text.replace("\"", "\\\"").replace("\n", "\\n")
+        Log.d("textToSpeech", "escapedText: $escapedText")
 
-        val json = """
-            {
-                "text": "$text",
-                "model_id": "eleven_multilingual_v2",
-                "voice_settings": {
-                    "stability": 0.5,
-                    "similarity_boost": 0.8,
-                    "style": 0.0,
-                    "use_speaker_boost": true
-                }
-            }
+        // Create the JSON string with proper formatting
+        val jsonBody = """
+        {
+            "model": "tts-1",
+            "input": "$escapedText",
+            "voice": "alloy"
+        }
         """.trimIndent()
 
-        val requestBody = json.toRequestBody("application/json".toMediaType())
+        // Log the JSON string for debugging
+        Log.d("TextToSpeech", "Request Body: $jsonBody")
+
+        // Convert the JSON string to RequestBody
+        val requestBody: RequestBody = jsonBody.toRequestBody(mediaType)
+
+        // Build the request
         val request = Request.Builder()
-            .url(ttsUrl)
+            .url("https://api.openai.com/v1/audio/speech")
             .post(requestBody)
-            .headers(headers)
+            .addHeader("Content-Type", "application/json")
+            .addHeader("Authorization", API)
             .build()
+
+        Log.d("TextToSpeech", "Request URL: ${request.url}")
+        Log.d("TextToSpeech", "Request Headers: ${request.headers}")
 
         try {
             val response = client.newCall(request).await()
-
             if (response.isSuccessful) {
+                var totalBytesRead = 0
                 response.body?.let { responseBody ->
                     val file = File(filesDir, OUTPUT_FILENAME)
                     FileOutputStream(file).use { outputStream ->
@@ -227,13 +235,112 @@ class DemoActivity : AppCompatActivity() {
                             var bytesRead: Int
                             while (inputStream.read(buffer).also { bytesRead = it } != -1) {
                                 outputStream.write(buffer, 0, bytesRead)
+                                totalBytesRead += bytesRead
                             }
                         }
                     }
-                    playAudio(file)
+
+                    // Get audio information
+                    val duration = MediaPlayer().apply { setDataSource(file.path) }.duration
+                    val fileSize = file.length()
+                    val numChunks = totalBytesRead / CHUNK_SIZE
+
+                    // Log audio information
+                    Log.d("textToSpeech", "Audio Duration: $duration ms")
+                    Log.d("textToSpeech", "Audio File Size: $fileSize bytes")
+                    Log.d("textToSpeech", "Number of Chunks: $numChunks")
+
+                    // Check if audio is complete
+                    val expectedFileSize = duration * bitrate / 8
+                    if (fileSize < expectedFileSize) {
+                        Log.e("textToSpeech", "Audio file is incomplete.")
+                    } else {
+                        playAudio(file)
+                    }
                 } ?: println("Failed to get response body.")
             } else {
-                println("Request failed: ${response.message}")
+                Log.e("TextToSpeech", "Request failed: ${response.message}")
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+            Log.e("TextToSpeech", "Error during request: ${e.message}")
+        }
+  }
+
+    private suspend fun textToSpeech(text: String) {
+        val client = OkHttpClient()
+        val mediaType = "application/json".toMediaType()
+        val escapedText = text.replace("\"", "\\\"").replace("\n", "\\n")
+        Log.d("textToSpeech", "escapedText: $escapedText")
+        val requestBodyString = """
+    {
+        "text": "$escapedText",
+        "model_id": "eleven_multilingual_v2",
+        "voice_settings": {
+            "stability": 0.5,
+            "similarity_boost": 0.8,
+            "style": 0.0,
+            "use_speaker_boost": true
+        }
+    }
+    """.trimIndent()
+
+        // Log the JSON string
+        Log.d("textToSpeech", "Request Body: $requestBodyString")
+
+        // Convert the JSON string to RequestBody
+        val requestBody = requestBodyString.toRequestBody(mediaType)
+
+        val request = Request.Builder()
+            .url("https://api.elevenlabs.io/v1/text-to-speech/$VOICE_ID/stream")
+            .post(requestBody)
+            .addHeader("xi-api-key", XI_API_KEY)
+            .addHeader("Content-Type", "application/json")
+            .build()
+
+        // Log the request details
+        Log.d("textToSpeech", "Request URL: ${request.url}")
+        Log.d("textToSpeech", "Request Headers: ${request.headers}")
+
+        try {
+            val response = client.newCall(request).await()
+
+            if (response.isSuccessful) {
+                var totalBytesRead = 0
+
+                response.body?.let { responseBody ->
+                    val file = File(filesDir, OUTPUT_FILENAME)
+                    FileOutputStream(file).use { outputStream ->
+                        responseBody.byteStream().use { inputStream ->
+                            val buffer = ByteArray(CHUNK_SIZE)
+                            var bytesRead: Int
+                            while (inputStream.read(buffer).also { bytesRead = it } != -1) {
+                                outputStream.write(buffer, 0, bytesRead)
+                                totalBytesRead += bytesRead
+                            }
+                        }
+                    }
+
+                    // Get audio information
+                    val duration = MediaPlayer().apply { setDataSource(file.path) }.duration
+                    val fileSize = file.length()
+                    val numChunks = totalBytesRead / CHUNK_SIZE
+
+                    // Log audio information
+                    Log.d("textToSpeech", "Audio Duration: $duration ms")
+                    Log.d("textToSpeech", "Audio File Size: $fileSize bytes")
+                    Log.d("textToSpeech", "Number of Chunks: $numChunks")
+
+                    // Check if audio is complete
+                    val expectedFileSize = duration * bitrate / 8
+                    if (fileSize < expectedFileSize) {
+                        Log.e("textToSpeech", "Audio file is incomplete.")
+                    } else {
+                        playAudio(file)
+                    }
+                } ?: println("Failed to get response body.")
+            } else {
+                println("Request failed: $response")
             }
         } catch (e: IOException) {
             e.printStackTrace()
@@ -255,11 +362,35 @@ class DemoActivity : AppCompatActivity() {
 
     private fun playAudio(file: File) {
         val mediaPlayer = MediaPlayer()
-        mediaPlayer.setDataSource(file.path)
-        mediaPlayer.prepare()
-        mediaPlayer.start()
-        mediaPlayer.setOnCompletionListener {
-            file.delete()
+        try {
+            Log.d("playAudio", "Playing audio from: ${file.path}")
+            mediaPlayer.setDataSource(file.path)
+            mediaPlayer.prepare()
+            mediaPlayer.start()
+
+            // Handler to log the playback position every second
+            val handler = Handler(Looper.getMainLooper())
+            val runnable = object : Runnable {
+                override fun run() {
+                    if (mediaPlayer.isPlaying) {
+                        Log.d("playAudio", "Current position: ${mediaPlayer.currentPosition / 1000} seconds")
+                        handler.postDelayed(this, 1000)
+                    }
+                }
+            }
+
+            // Start logging playback position
+            handler.post(runnable)
+
+            mediaPlayer.setOnCompletionListener {
+                Log.d("playAudio", "Audio completed, deleting file: ${file.path}")
+                it.release()
+                file.delete()
+                handler.removeCallbacks(runnable)
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+            Log.e("playAudio", "Error playing audio: ${e.message}")
         }
     }
 
